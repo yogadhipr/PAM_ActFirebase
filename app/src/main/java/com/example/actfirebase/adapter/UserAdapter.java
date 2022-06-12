806319bbdc33f8d9198573edf796d99e088fdde6
Yoga Adhi Prasetya <yoga0287@gmail.com>
2022-06-12 10:22:42 +0700

    PAM Act Firebase

---
+++ app/src/main/java/com/example/actfirebase/adapter/UserAdapter.java
@@ -0,0 +1,73 @@
+package com.example.actfirebase.adapter;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.actfirebase.R;
+import com.example.actfirebase.model.User;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> {
+    private Context context;
+    private List<User> list;
+    private Dialog dialog;
+
+    public interface Dialog{
+        void OnClick(int pos);
+    }
+
+    public void setDialog(Dialog dialog) {
+        this.dialog = dialog;
+    }
+
+    public UserAdapter(Context context, List<User> List){
+        this.context = context;
+        this.list = list;   
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        boolean attachToRoot;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_user, parent,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.name.setText(list.get(position).getName());
+        holder.email.setText(list.get(position).getEmail());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            email = itemView.findViewById(R.id.email);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (dialog != null) {
+                        dialog.OnClick(getLayoutPosition());
+                    }
+                }
+            });
+        }
+    }
+}
